61045ac3926017cda49612e62e026dce8f051a58
ritaortese <94189719+ritaortese@users.noreply.github.com>
2026-06-20 18:44:05 +0200

    calculate the possible weights for the multi tree

--- utils/plot_tree.py
+++ utils/plot_tree.py
@@ -45,43 +45,5 @@ def print_tree(t, name):
     d.view()
     
     
-    
-def print_tree_second(t, name):
-    # Remove the "produced/" prefix from here, so it will be controlled it from the main script
-    if name.endswith(".gv"):
-        filename = name[:-3]
-    else:
-        filename = name
+import graphviz
 
-    d = graphviz.Digraph(filename=filename, format="pdf")
-
-    # layout
-    d.attr(rankdir="TB")         # from L to R tree
-    d.attr(ranksep="2.5")        # Distance bt ranks
-    d.attr(nodesep="0.4")        #distance bt nodes
-       
-    # color and shape of the cell
-    d.attr("node", shape="box", style="rounded,filled", fillcolor="aliceblue", 
-           fontname="Helvetica", fontsize="11", margin="0.2,0.1")
-
-    for k, v in t.items():
-        # Graphviz aligns the cell without the box
-        with d.subgraph(name=f"rank_{k}") as s:
-            s.attr(rank="same")
-            for label_name in v:
-                s.node(label_name, label=label_name)
-
-    # Add edges
-    d.attr("edge", color="gray75", penwidth="0.8", arrowsize="0.6")
-    
-    max_rank = max(t.keys())
-    for r in range(1, max_rank):
-        if r in t and (r + 1) in t:
-            for n1 in t[r]:
-                for n2 in t[r + 1]:
-                    d.edge(n1, n2)
-
-    # Save the file (which automatically generates the .gv and .pdf)
-    d.render(cleanup=False) 
-    d.view
-    print(f"Graph rendered: {filename}.gv and {filename}.pdf")
